d1b4492f496944371cd0c5e7dc209a87cd82f43f
Anthony Diaz <anthonydiaz_48@yahoo.com>
2021-09-16 13:36:24 -0700

    Update quit button's id. Clicking quit button will display a Bye toast message

--- app/src/main/java/com/example/cspc_544_homework_1/MainActivity.kt
+++ app/src/main/java/com/example/cspc_544_homework_1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.cspc_544_homework_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun onQuitButtonClicked(view: View) {
+
+        val toastMessage = Toast.makeText(this@MainActivity, "Bye", Toast.LENGTH_SHORT)
+//        toastMessage.setGravity(Gravity.TOP, 0, 0)
+        toastMessage.show()
     }
 }
